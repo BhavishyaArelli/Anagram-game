a57f9f62838f53d5e1f1fee2c657807319fa924d
bhavishya <s528734@nwmissouri.edu>
2017-09-15 11:43:02 -0500

    new word added

--- AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,6 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "ahbsivayh",
         "nitahy",
         "uffp",
         "abstraction",
@@ -86,6 +87,7 @@ final class StaticWordLibrary extends WordLibrary {
         "traditional"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
+        "bhavishya",
         "nithya",
         "batsartcoin",
         "maibuguos",
